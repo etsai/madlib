3613bda2cdb57fd39f534a7dca692db917fac240
Elaine Tsai <et.elainetsai@gmail.com>
2012-01-22 18:18:56 -0800

    Made app.rb the Madlib Sinatra app. Add config.ru.

--- Gemfile
+++ Gemfile
@@ -1,12 +1,12 @@
 source "http://rubygems.org"
 
+gem "sinatra", :require => ["sinatra/base"]
 gem "data_mapper"
 gem "dm-sqlite-adapter"
 
 group :production do
   "dm-postgres-adapter"
 end
-
 
 group :test do 
   gem 'minitest'
--- app.rb
+++ app.rb
@@ -8,3 +8,17 @@ require_relative "models/story"
 # Run any database migrations
 DataMapper.finalize
 DataMapper.auto_upgrade!
+
+Story.create :text => "A fine :word1 story"
+
+Story.create :text => "A new :word1 story"
+
+Story.create :text => "An old :word1 story"
+
+Story.create :text => "A lousy :word1 story"
+
+class Madlib < Sinatra::Base
+  get "/stories/:story_id" do
+    Story.get(params[:story_id]).text
+  end
+end
