a9bceb9a7ef8438983e719f83f7d0dcda41aff7f
Steven! Ragnarök <steven@nuclearsandwich.com>
2012-01-24 23:21:04 -0800

    Add tests for Story#add_word and Presenters.

--- tests/story_presenter_test.rb
+++ tests/story_presenter_test.rb
@@ -4,6 +4,8 @@ require_relative "../app"
 class StoryPresenterTest < MiniTest::Unit::TestCase
   def setup
     @story = Story.new :text => "This is a :word1 story."
+    @word = Word.new :url => "word1.mp3", :type => "word"
+    @story.add_word @word
     @sp = StoryPresenter.new @story
   end
 
@@ -15,10 +17,15 @@ class StoryPresenterTest < MiniTest::Unit::TestCase
     assert_equal @story, @sp.story
   end
 
-  def test_story_presenter_uses_String
+  def test_story_presenter_telling_xml_says_a_story
     @story = Story.new :text => "This is a story."
     @sp = StoryPresenter.new @story
 
     assert_equal "<Say>This is a story.</Say>", @sp.telling_xml
+  end
+
+  def test_story_presenter_telling_xml_plays_words
+    assert_equal "<Say>This is a </Say><Play>word1.mp3</Play><Say> story.</Say>",
+      @sp.telling_xml
   end
 end
