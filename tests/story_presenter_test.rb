0f51969dbf2cb55dfb801ff7c9f255e75b7016eb
Steven! Ragnarök <steven@nuclearsandwich.com>
2012-01-24 21:33:50 -0800

    Remove builder and fix tests.

--- tests/story_presenter_test.rb
+++ tests/story_presenter_test.rb
@@ -10,14 +10,15 @@ class StoryPresenterTest < MiniTest::Unit::TestCase
   def test_story_presenter_is_created
     assert @sp
   end
-  
+
   def test_story_presenter_has_story
     assert_equal @story, @sp.story
   end
-  
+
   def test_story_presenter_uses_String
     @story = Story.new :text => "This is a story."
     @sp = StoryPresenter.new @story
-    assert_equal "<Say>This is a story</Say>", @sp.telling_xml
+
+    assert_equal "<Say>This is a story.</Say>", @sp.telling_xml
   end
 end
